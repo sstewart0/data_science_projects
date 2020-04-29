450a23f9ae255fb4b6acd3795874a6a871880600
sstewart0 <63207039+sstewart0@users.noreply.github.com>
2020-04-29 14:23:31 +0100

    update whodied

--- titanic/who_died.R
+++ titanic/who_died.R
@@ -104,10 +104,9 @@ train$Fare <- as.numeric(scale(train$Fare))
   # -> is there a pattern in missing values. There are 177/891 missing age values
   # -> regress age onto Survived, Pclass, SibSp, Title
   # -> train: Non-NA,   test: NA
-
 age.test <- train[which(is.na(train$Age)),]
 age.train <- train[which(!is.na(train$Age)),]
-
+View(age.train)
 #replace age with nonscaled age
 
 View(age.train)
@@ -125,12 +124,6 @@ bptest(lm.age)
 lm.age2 <- lm(sqrt(Age)~Survived+Pclass+SibSp+Title,data = age.train)
 plot(lm.age2)
 #Observations from lm.age2: residuals-vs-fitted => much less heteroskedasticity
-  #however nonlinearity may exist. Solution: Add higher order terms for Pclass and/or SipSp?
-    #chosen model below
-lm.age5 <- lm(sqrt(Age)~Survived+poly(Pclass,2)+poly(SibSp,2)+Title,data = age.train)
-summary(lm.age5)
-plot(lm.age5)
-#Still not happy with above model because the BP test still "detects" heteroskedasticity
 #need to look into WLS and/or GLS
 
 #predictions for test set (i.e. NA values)
@@ -139,24 +132,103 @@ pred <- round(pred^2,0)
 train[is.na(train$Age),]$Age <- pred
 
 #training error
-pred.train <- predict(lm.age5)
+pred.train <- predict(lm.age2)
 pred.t <- round(pred.train^2,1)
 sse <- sum((as.numeric(pred.t)-as.numeric(age.train$Age))^2)
-mse<-sse/(nrow(age.train)-7)
+mse1<-sse/(nrow(age.train)-7)
+
 #Scale the age variable now
 train$Age <- as.numeric(scale(train$Age))
-summary(train2[is.na(train2$Age),])
 
-nrow(age.test[age.test$Survived==0,])/nrow(age.test) #0.7062147
-nrow(train[train$Survived==0,])/nrow(train) #0.6161616
+#Age -vs- survival and Age -vs- Pclass
+library(ggplot2)
+ggplot(data=train2, aes(x=Age, group=Pclass, fill=Pclass)) +
+  geom_density(adjust=1.5, alpha=.4)
+#Age follows normal distribution in each class however each have different mean,sd.
+#it appears that ages from pclass 2,3 could be drawn from the same (normal) distribution
+#So; fit models separately to predict Age for each class and assess training error.
 
-nrow(age.test[age.test$Pclass==1,])/nrow(age.test)#0.1694915
-nrow(train[train$Pclass==1,])/nrow(train)#0.2424242
+  #pclass==1
+age.pclass1 <- lm(Age~Survived+Title+Parch,data = age.train[age.train$Pclass==1,])
+summary(age.pclass1)
+plot(age.pclass1)#heteroskedasticity possibly present
+bptest(age.pclass1)#BP = 8.0288, df = 5, p-value = 0.1547 => cannot reject H_0:homoskedasticity
+#assess training error
+p1<-predict(age.pclass1)
+mse1 <- (sum((as.numeric(p1)-as.numeric(age.train[age.train$Pclass==1,]$Age))^2))/(nrow(age.train)-6)#44.188
 
-nrow(age.test[age.test$Pclass==2,])/nrow(age.test)#0.06214689
-nrow(train[train$Pclass==2,])/nrow(train)#0.2065095
+  #pclass==2,3
+age.pclass23 <- lm(Age~Survived+Title+SibSp+Embarked,data = age.train[age.train$Pclass!=1,])
+summary(age.pclass23)
+plot(age.pclass23)#some funneling, no non-linearity
+bptest(age.pclass2)#BP = 11.947, df = 6, p-value = 0.06317 => cannot reject H_0:homoskedasticity at alpha=0.05
+#assess training error
+p23<-predict(age.pclass23)
+summary(p23)
+mse23 <- (sum((as.numeric(p23)-as.numeric(age.train[age.train$Pclass!=1,]$Age))^2))/(nrow(age.train)-8)
 
-nrow(age.test[age.test$Pclass==3,])/nrow(age.test)#0.7683616
-nrow(train[train$Pclass==3,])/nrow(train)#0.5510662
-boxplot(train2[train2$Pclass==3,]$Age)
+#y ~ ... ; mse = 79.25 #negative ages present!!
+#sqrt(y) ~ ... ; mse = 80.33
+
+#pclass==2
+age.pclass2 <- lm(Age~Title+SibSp+Embarked,data = age.train[age.train$Pclass==2,])
+summary(age.pclass2)
+plot(age.pclass2)#funneling, some non-linearity
+bptest(age.pclass2)#BP = 11.947, df = 6, p-value = 0.06317 => cannot reject H_0:homoskedasticity at alpha=0.05
+#assess training error
+p2<-predict(age.pclass2)
+summary(p2)
+mse2 <- (sum((as.numeric(p2)-as.numeric(age.train[age.train$Pclass==2,]$Age))^2))/(nrow(age.train)-7)#30.25
+
+
+#pclass==3
+age.pclass3 <- lm(sqrt(Age)~Title+SibSp+Embarked+Survived,data = age.train[age.train$Pclass==3,])
+summary(age.pclass3)
+plot(age.pclass3)#very little/no funneling, possible non-linearity
+bptest(age.pclass3)
+
+#BP = 15.117, df = 7, p-value = 0.03453 => reject H_0
+#try sqrt(y): BP = 12.338, df = 7, p-value = 0.08998 => do not reject H_0. better model, no more negative ages
+
+#assess training error
+p3<-predict(age.pclass3)^2
+summary(p3)
+mse3 <- (sum((as.numeric(p3)-as.numeric(age.train[age.train$Pclass==3,]$Age))^2))/(nrow(age.train)-8)#45.51
+
+#average mse:
+(mse1+mse2+mse3)/3 #39.99 much better result than fitting together but still not happy; 
+
+#add cabin Y/N and refit?
+cab<-rep(c(1),891)
+train<-cbind(train,cab)
+train[train$Cabin=="",]$cab<-c(0)
+train$Age<-train2$Age
+age.train <- train[which(!is.na(train$Age)),]
+age.test <- train[which(is.na(train$Age)),]
+
+#pclass==3
+new.age.pclass3 <- lm(sqrt(Age)~Title+SibSp+Embarked+Survived+Survived+cab,data = age.train[age.train$Pclass==3,])
+summary(new.age.pclass3)
+View(new.age.pclass3$fitted.values^2)
+plot(new.age.pclass3)#looks ok?
+bptest(new.age.pclass3)#BP = 15.926, df = 8, p-value = 0.04345, add sqrt! BP = 9.8806, df = 8, p-value = 0.2735 => uh huh eskeetit
+#transformed model has MSE = (0.956)^2 !!!
+
+
+#predict NA ages:
+na.pred1<-predict(age.pclass1,age.test[age.test$Pclass==1,])
+na.pred2<-predict(age.pclass2,age.test[age.test$Pclass==2,])
+#need to correct for E[y] since we took sqrt
+na.pred3<-(predict(new.age.pclass3,age.test[age.test$Pclass==3,]))^2+sigma(new.age.pclass3)^2
+
+#happy with ages from pclass 1 but not 2 or 3. H_0: underage
+View(train[which(!is.na(train$Age)&train$Pclass!=1),])
+train$Age<-train2$Age
+
+train[is.na(train$Age)&train$Pclass==1,]$Age <- round(na.pred1,1)
+train[is.na(train$Age)&train$Pclass==2,]$Age <- round(na.pred2,1)
+train[is.na(train$Age)&train$Pclass==3,]$Age <- round(na.pred3,1)
+View(train)
+
+
 
