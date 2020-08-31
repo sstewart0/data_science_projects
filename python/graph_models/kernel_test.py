195818943cc3d213603cf97e2dbcc38311661f8a
sstewart0 <63207039+sstewart0@users.noreply.github.com>
2020-08-31 13:31:13 +0100

    Updates
    Updated kernels

--- python/graph_models/kernel_test.py
+++ python/graph_models/kernel_test.py
@@ -7,8 +7,42 @@ def main():
     network = ba.initialise(5)
     ba.add_n_nodes(network, 5)
     similarity_matrix = krn.sim_matrix(network, method="adj")
-    kernel = krn.von_neumann_diff_kernel(similarity_matrix, .1)
 
+    kernel1 = krn.power_kernel(similarity_matrix, 2)
+    kernel2 = krn.exp_diff_kernel(similarity_matrix, .1)
+    kernel3 = krn.von_neumann_diff_kernel(similarity_matrix, .1)
+
+    np.set_printoptions(suppress=True)
+
+    '''
+    print(np.round(krn.norm(kernel1), 2))
+    print(np.round(krn.norm(kernel2), 2))
+    print(np.round(krn.norm(kernel3), 2))
+    
+    print(np.round(krn.sq_distance(kernel1), 2))
+    print(np.round(krn.sq_distance(kernel2), 2))
+    print(np.round(krn.sq_distance(kernel3), 2))
+    
+    print(np.round(krn.sq_norm_mean(kernel1), 2))
+    print(np.round(krn.sq_norm_mean(kernel2), 2))
+    print(np.round(krn.sq_norm_mean(kernel3), 2))
+    
+    print(np.round(krn.distance_to_mean(kernel1), 2))
+    print(np.round(krn.distance_to_mean(kernel2), 2))
+    print(np.round(krn.distance_to_mean(kernel3), 2))
+    
+    print(np.round(krn.tot_var(kernel1), 2))
+    print(np.round(krn.tot_var(kernel2), 2))
+    print(np.round(krn.tot_var(kernel3), 2))
+    
+    print(np.round(krn.centred_kernel(kernel1), 2))
+    print(np.round(krn.centred_kernel(kernel2), 2))
+    print(np.round(krn.centred_kernel(kernel3), 2))
+    
+    print(np.round(krn.normalise_kernel(kernel1), 2))
+    print(np.round(krn.normalise_kernel(kernel2), 2))
+    print(np.round(krn.normalise_kernel(kernel3), 2))
+    '''
 
 
 if __name__ == "__main__":
--- python/graph_models/kernels.py
+++ python/graph_models/kernels.py
@@ -42,12 +42,18 @@ def power_kernel(s, walk_length):
     pos_semi_def = is_symmetric(s)
     if odd_length:
         if pos_semi_def:
-            return np.linalg.matrix_power(s, walk_length)
+            vals, vec = np.linalg.eig(s)
+            diag_vals = np.diag(vals)
+            return vec.dot(np.linalg.matrix_power(diag_vals, walk_length)).dot(
+                np.transpose(vec))
         else:
             print("Similarity matrix is not positive semi-definite")
     # if walk length is even we are guaranteed a valid (i.e. positive semi-definite) kernel
     else:
-        return np.linalg.matrix_power(s, walk_length)
+        vals, vec = np.linalg.eig(s)
+        diag_vals = np.diag(vals)
+        return vec.dot(np.linalg.matrix_power(diag_vals, walk_length)).dot(
+            np.transpose(vec))
 
 
 # Function that produces an exponential diffusion kernel using the provided similarity matrix s
@@ -61,11 +67,73 @@ def exp_diff_kernel(s, damping_factor):
 
 # Function that produces the Von Neumann Diffusion Kernel using the provided similarity matrix s
 def von_neumann_diff_kernel(s, damping_factor):
-    eigen_vals = np.linalg.eigvals(s)
-    n = len(eigen_vals)
-    spectral_radius = eigen_vals.flat[abs(eigen_vals).argmax()]
+    vals, vec = np.linalg.eig(s)
+    spectral_radius = vals.flat[abs(vals).argmax()]
     if abs(damping_factor) < 1/spectral_radius:
-        return np.linalg.inv(np.identity(n) - s*damping_factor)
+        identity = np.identity(len(vals))
+        diag_vals = np.diag(vals)
+        diagonal = np.linalg.inv(identity - damping_factor*diag_vals)
+        return vec.dot(diagonal).dot(np.transpose(vec))
     else:
         print("Damping factor must be smaller than ", 1/spectral_radius)
     return None
+
+
+# Given a kernel matrix we can perform operations in the feature space without explicitly
+# stating the map (from input to feature space) Ω : I -> F.
+# We know that the valid kernel is equivalent to the inner product in some feature space F
+
+# Function to compute norm in feature space
+def norm(kernel):
+    if kernel is not None:
+        return np.array([[math.sqrt(x) if round(x, 5) > 0 else 0 for x in y] for y in kernel])
+    else:
+        print("Kernel is not valid")
+
+
+# Function to compute the squared distances between all pairs of points in the feature space
+def sq_distance(kernel):
+    n, m = np.shape(kernel)
+    distances = np.zeros((n, n))
+    for i, row in enumerate(kernel):
+        for j, element in enumerate(row):
+            distances[i][j] = kernel[i][i]+kernel[j][j]-2*kernel[i][j]
+    return distances
+
+
+# Function to compute squared norm of the mean in the feature space
+def sq_norm_mean(kernel):
+    n, m = np.shape(kernel)
+    return (1/n**2)*np.sum(kernel)
+
+
+# Function to compute distance between each point and the mean in the feature space
+def distance_to_mean(kernel):
+    n, m = np.shape(kernel)
+    a = (2/n)*np.sum(kernel, axis=1)
+    b = (1/n**2)*np.ones((1, n))*np.sum(kernel)
+    return np.diag(kernel)-a+b
+
+
+# Function to compute the total variance in the feature space
+def tot_var(kernel):
+    n, m = np.shape(kernel)
+    return (1/n)*np.sum(np.diag(kernel)) - (1/n**2)*np.sum(kernel)
+
+
+# Function to produce a centred kernel matrix
+def centred_kernel(kernel):
+    n, m = np.shape(kernel)
+    centring = np.identity(n) - (1/n)*np.ones((n, n))
+    return centring.dot(kernel).dot(centring)
+
+
+# Function to normalise the kernel matrix
+def normalise_kernel(kernel):
+    n, m = np.shape(kernel)
+    diag = np.zeros((n, n))
+    np.fill_diagonal(diag, np.diag(kernel))
+    print(diag)
+    normaliser = np.linalg.inv(np.sqrt(diag))
+    print(normaliser)
+    return normaliser.dot(kernel).dot(normaliser)
